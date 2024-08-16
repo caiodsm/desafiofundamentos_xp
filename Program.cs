8bcc23087d02a470ab27606e15e896f5062b4c80
Caio Miranda <caiomirand@gmail.com>
2024-08-16 11:19:02 -0300

    desafio finalizado e implementação complementar

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using Desafio.Models;
+
+// Coloca o encoding para UTF8 para exibir acentuação
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+decimal precoInicial = 0;
+decimal precoPorHora = 0;
+
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
+                  "Digite o preço inicial:");
+precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Agora digite o preço por hora:");
+precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
+Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
+
+string opcao = string.Empty;
+bool exibirMenu = true;
+
+// Realiza o loop do menu
+while (exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículos");
+    Console.WriteLine("4 - Encerrar");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            es.AdicionarVeiculo();
+            break;
+
+        case "2":
+            es.RemoverVeiculo();
+            break;
+
+        case "3":
+            es.ListarVeiculos();
+            break;
+
+        case "4":
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+    
+    if(exibirMenu)
+    {
+        Console.WriteLine("Digite a sua opção:");
+        Console.WriteLine("Deseja Voltar ao Menu ? - Digite 's'");
+        Console.WriteLine("Deseja encerrar ? - Digite 'n'");
+        
+        bool errado = false;
+        while(!errado)
+        {
+            string resposta =  Convert.ToString(Console.ReadLine());
+            if(resposta == "s")
+            {
+                exibirMenu = true;
+                errado = true;
+            } else if (resposta == "n") 
+            {
+                exibirMenu = false;
+                errado = true;
+            } else {
+                Console.WriteLine("Opção inválida, tente novamente s ou n");
+                errado = false;
+            }
+        }
+        
+    }
+}
+
+Console.WriteLine("O programa se encerrou");
